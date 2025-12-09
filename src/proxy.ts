89f018b1666fdc0d19e836ac10ffbb0f8712f728
samson <ssafari@verst.earth>
2025-12-09 10:32:47 +0300

    addedd jenkinsfile

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,12 +8,11 @@ export default clerkMiddleware(async (auth, req: NextRequest) => {
     console.log('Protecting route:', req.nextUrl.pathname);
   }
 });
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes, but exclude health endpoint
-    '/(api|trpc)(?!.*/health)(.*)'
+    // Always run for API routes
+    '/(api|trpc)(.*)'
   ]
 };
