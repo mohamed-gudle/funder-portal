fb8766df6e4fa7dc4ce7988a50a20bd0abd8ffe3
Mohamed Gudle <gudlemohamed@gmail.com>
2025-12-09 14:15:34 +0300

    feat: update background color and remove placeholder quote in sign-in view

--- src/features/auth/components/sign-in-view.tsx
+++ src/features/auth/components/sign-in-view.tsx
@@ -11,20 +11,11 @@ export default function SignInViewPage() {
   return (
     <div className='relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
       <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r'>
-        <div className='absolute inset-0 bg-zinc-900' />
+        <div className='bg-primary absolute inset-0' />
         <div className='relative z-20 flex items-center text-lg font-medium'>
           <Image src='/logo.svg' alt='logo' width={28} height={28} />
         </div>
-        <div className='relative z-20 mt-auto'>
-          <blockquote className='space-y-2'>
-            <p className='text-lg'>
-              &ldquo;This starter template has saved me countless hours of work
-              and helped me deliver projects to my clients faster than ever
-              before.&rdquo;
-            </p>
-            <footer className='text-sm'>Random Dude</footer>
-          </blockquote>
-        </div>
+        <div className='relative z-20 mt-auto'></div>
       </div>
       <div className='flex h-full items-center justify-center p-4 lg:p-8'>
         <SignInForm />
